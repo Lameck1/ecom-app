51c8d2e6c3a0ac2efde1442b9383ffc6cd520ff8
Lameck1 <lameck72@gmail.com>
2022-08-10 14:51:59 +0300

    Refactor Header component for mobile first design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,50 +4,68 @@ import styles from './Header.module.scss';
 import logo from '../../assets/logo.png';
 import { BiSearchAlt2 } from 'react-icons/bi';
 import { FaShoppingCart } from 'react-icons/fa';
+import { FaUserAlt } from 'react-icons/fa';
 import Account from './Account';
 import Help from './Help';
+
+const search = (value, handleChange) => (
+  <div className={styles.search}>
+    <BiSearchAlt2
+      style={{ color: '#333', fontSize: '1.3rem', padding: '0 0.5rem' }}
+    />
+    <input
+      type='text'
+      value={value}
+      onChange={handleChange}
+      placeholder='Search products'
+    />
+  </div>
+);
+
+const cart = (
+  <div className={styles.cart}>
+    <Link to='/cart' className={styles['cart-link']}>
+      <span>Cart</span> <FaShoppingCart />
+      <span className={styles['cart-count']}>0</span>
+    </Link>
+  </div>
+);
 
 function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles['logo-container']}>
-        <div className={styles.humburger}>
-          <span></span>
-          <span></span>
-          <span></span>
+      <>
+        <div className={styles['top-header']}>
+          <div className={styles['logo-container']}>
+            <div className={styles.humburger}>
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+            <Link to='/' className={styles['logo-anchor']}>
+              <img src={logo} alt='Ecom logo' className={styles.logo} />
+            </Link>
+          </div>
+          <div className={styles['top-header-icons']}>
+            <Link to='/' className={styles['menu-item']}>
+              <FaUserAlt />
+            </Link>
+            {cart}
+          </div>
         </div>
-        <Link to='/' className={styles['logo-anchor']}>
-          <img src={logo} alt='Ecom logo' className={styles.logo} />
-        </Link>
-      </div>
-      <form
-      // onSubmit={this.handleSubmit}
-      >
-        <div className={styles.search}>
-          <BiSearchAlt2
-            style={{ color: '#333', fontSize: '1.3rem', padding: '0 0.5rem' }}
-          />
-          <input
-            type='text'
-            // value={value}
-            // onChange={handleChange}
-            placeholder='Search products'
-          />
-        </div>
-
-        <input type='submit' value='search' />
-      </form>
-
-      <nav className={styles.nav}>
-        <Account />
-        <Help />
-        <div className={styles.cart}>
-          <Link to='/cart' className={styles['cart-link']}>
-            <span>Cart</span> <FaShoppingCart />
-            <span className={styles['cart-count']}>0</span>
-          </Link>
-        </div>
-      </nav>
+        <form
+        // onSubmit={this.handleSubmit}
+        >
+          {search()}
+          <input type='submit' value='search' />
+        </form>
+        <nav className={styles.nav}>
+          <Account />
+          <Help />
+          {cart}
+        </nav>
+      </>
+      {search()}
     </header>
   );
 }
