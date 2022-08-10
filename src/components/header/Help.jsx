6f5e0e1dfc4774f40e12da348e5ff72a84624542
Lameck1 <lameck72@gmail.com>
2022-08-10 08:38:57 +0300

    Create Help dropdown menu component

---
+++ src/components/header/Help.jsx
@@ -0,0 +1,45 @@
+import React, { useRef } from 'react';
+import { BiHelpCircle } from 'react-icons/bi';
+import styles from './Header.module.scss';
+import { Link } from 'react-router-dom';
+import { IoIosArrowDown, IoIosArrowUp } from 'react-icons/io';
+import useOnClickOutside from '../../customHooks/useOnClickOutside';
+
+function Help() {
+  const [isOpen, setIsOpen] = React.useState(false);
+
+  const helpMenu = useRef(null);
+  useOnClickOutside(helpMenu, () => setIsOpen(false));
+
+  return (
+    <div
+      ref={helpMenu}
+      className={styles.menu}
+      onClick={() => setIsOpen(!isOpen)}
+      // onMouseEnter={() => setIsOpen(true)}
+      // onMouseLeave={() => setIsOpen(false)}
+    >
+      <BiHelpCircle />
+      <span>Help</span>
+      {isOpen ? <IoIosArrowUp /> : <IoIosArrowDown />}
+      <div
+        className={isOpen ? styles['menu-items'] : styles['menu-items-hidden']}
+      >
+        <Link to='/help-center' className={styles['menu-item']}>
+          Help Center
+        </Link>
+        <Link to='/order-cancelation' className={styles['menu-item']}>
+          Order Cancellation
+        </Link>
+        <Link to='/returns-and-refunds' className={styles['menu-item']}>
+          Returns and Refunds
+        </Link>
+        <Link to='/contact' className={styles['menu-item']}>
+          Contact Us
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Help;
