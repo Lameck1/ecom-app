5ad6fd43d27a68c986d6028efaf9a450978c515e
Lameck1 <lameck72@gmail.com>
2022-08-10 08:39:14 +0300

    Create Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,55 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.scss';
+import logo from '../../assets/logo.png';
+import { BiSearchAlt2 } from 'react-icons/bi';
+import { FaShoppingCart } from 'react-icons/fa';
+import Account from './Account';
+import Help from './Help';
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header className={styles.header}>
+      <div className={styles['logo-container']}>
+        <div className={styles.humburger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <Link to='/' className={styles['logo-anchor']}>
+          <img src={logo} alt='Ecom logo' className={styles.logo} />
+        </Link>
+      </div>
+      <form
+      // onSubmit={this.handleSubmit}
+      >
+        <div className={styles.search}>
+          <BiSearchAlt2
+            style={{ color: '#333', fontSize: '1.3rem', padding: '0 0.5rem' }}
+          />
+          <input
+            type='text'
+            // value={value}
+            // onChange={handleChange}
+            placeholder='Search products'
+          />
+        </div>
+
+        <input type='submit' value='search' />
+      </form>
+
+      <nav className={styles.nav}>
+        <Account />
+        <Help />
+        <div className={styles.cart}>
+          <Link to='/cart' className={styles['cart-link']}>
+            <span>Cart</span> <FaShoppingCart />
+            <span className={styles['cart-count']}>0</span>
+          </Link>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
