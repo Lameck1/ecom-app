57377378cd1a7c89ebce1062dd78d55b138c6c04
Lameck1 <lameck72@gmail.com>
2022-08-19 08:00:14 +0300

    Create and Style footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import logo from '../../assets/logo.png';
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <img src={logo} alt='Ecom logo' className={styles.logo} />
+      <p> &copy; {new Date().getFullYear()} - All Rights Reserved </p>
+    </footer>
+  );
 }
 
 export default Footer;
