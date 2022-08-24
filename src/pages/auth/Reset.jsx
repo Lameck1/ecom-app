5892f98119b8c3a2160fc8c7203b685201cb4bc9
Lameck1 <lameck72@gmail.com>
2022-08-24 21:02:32 +0300

    Implement password reset logic

--- src/pages/auth/Reset.jsx
+++ src/pages/auth/Reset.jsx
@@ -1,24 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './auth.module.scss';
 import logo from '../../assets/logo_0.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { auth } from '../../firebase/config';
+import Loader from '../../components/loader/Loader';
+import { sendPasswordResetEmail } from 'firebase/auth';
 
 function Reset() {
-  const handleSubmit = (e) => {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleResetPassword = (e) => {
     e.preventDefault();
-    console.log('Submitted');
+    setIsLoading(true);
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        setIsLoading(false);
+        navigate('/login');
+        toast.success(`Successfully sent reset password email!`);
+      })
+      .catch((error) => {
+        toast.error(error.code.split('/')[1].split('-').join(' '));
+        setIsLoading(false);
+      });
+  };
+
+  const handleChange = (e) => {
+    setEmail(e.target.value);
   };
 
   return (
     <section>
+      {isLoading && <Loader />}
       <img src={logo} alt='logo' className={styles.logo} />
       <p>Recover your ECOM account!</p>
       <p>Please provide the correct email you used to create your account.</p>
       <h1>Reset password</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleResetPassword}>
         <div className={styles['form-group']}>
           <label htmlFor='email'>Email</label>
           <input
+            onChange={handleChange}
             type='email'
             className={styles['form-control']}
             id='email'
