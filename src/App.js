5e3521a4b5dce5ae6ffc143f16db3cf3c562c010
Lameck1 <lameck72@gmail.com>
2022-08-24 14:53:19 +0300

    Add toaster component to App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,27 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Contact, Home, Login, Register, Reset } from './pages';
 import { Header, Footer } from './components';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <BrowserRouter>
+      <Toaster
+        position='top-right'
+        reverseOrder={false}
+        toastOptions={{
+          success: {
+            style: {
+              border: '1px solid green',
+            },
+          },
+          error: {
+            style: {
+              border: '1px solid red',
+            },
+          },
+        }}
+      />
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
