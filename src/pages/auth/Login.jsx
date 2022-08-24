5ca49b1a05762ee2ede178eaa23cc2955dea9f3f
Lameck1 <lameck72@gmail.com>
2022-08-24 15:05:16 +0300

    Implement Login and Logout logic

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,38 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './auth.module.scss';
 import { FcGoogle } from 'react-icons/fc';
 import logo from '../../assets/logo_0.png';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../firebase/config';
+import Loader from '../../components/loader/Loader';
 
 function Login() {
-  const handleSubmit = (e) => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const loginUser = (e) => {
     e.preventDefault();
-    console.log('Submitted');
+    setIsLoading(true);
+    signInWithEmailAndPassword(auth, formData.email, formData.password)
+      .then((userCredential) => {
+        setIsLoading(false);
+        navigate('/');
+        toast.success(`Successfully logged in!`);
+      })
+      .catch((error) => {
+        toast.error(error.code.split('/')[1].split('-').join(' '));
+        setIsLoading(false);
+      });
   };
 
   return (
     <section>
+      {isLoading && <Loader />}
       <img src={logo} alt='logo' className={styles.logo} />
       <p>Welcome back!!</p>
       <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={loginUser}>
         <div className={styles['form-group']}>
           <label htmlFor='email'>Email</label>
           <input
+            onChange={handleChange}
             type='email'
             className={styles['form-control']}
             id='email'
             placeholder='Enter email'
+            name='email'
             required
           />
         </div>
         <div className={styles['form-group']}>
           <label htmlFor='password'>Password</label>
           <input
+            onChange={handleChange}
             type='password'
             className={styles['form-control']}
             id='password'
             placeholder='Enter password'
+            name='password'
             required
           />
         </div>
